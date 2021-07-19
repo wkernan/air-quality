f4ac4f01bcd04a14d3fefb675c764438c54538a1
Bill Kernan <wkernan@gmail.com>
2021-07-19 09:37:27 -0500

    Alert now shows for below or above aqi. Added to gitignore

--- client/src/components/AirQuality.js
+++ client/src/components/AirQuality.js
@@ -11,18 +11,30 @@ const useStyles = makeStyles((theme) => ({
 const AirQuality = (props) => {
   const classes = useStyles();
 
+  const isAboveThreshold = () => {
+    return props.aqi > props.userAqi;
+  };
+
+  const alertSeverity = () => {
+    return isAboveThreshold()
+      ? { severity: "error", message: "AQI is Above Your Threshold" }
+      : { severity: "success", message: "AQI is at or Below Your Threshold" };
+  };
+
   return (
     <Container align="center">
       <Typography variant="h2" align="center">
         Air Quality
       </Typography>
-      {props.aqi > props.userAqi && (
-        <Container maxWidth="sm">
-          <Alert variant="filled" severity="error" className={classes.alert}>
-            Warning AQI is Above Your Threshold
-          </Alert>
-        </Container>
-      )}
+      <Container maxWidth="sm">
+        <Alert
+          variant="filled"
+          severity={alertSeverity().severity}
+          className={classes.alert}
+        >
+          {alertSeverity().message}
+        </Alert>
+      </Container>
       <Typography variant="h2" align="center">
         {props.aqi}
       </Typography>
